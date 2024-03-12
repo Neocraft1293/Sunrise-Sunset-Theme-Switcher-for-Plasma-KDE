3fb4db1e906cee796869fe649d2bbeb8665a03ef
Nicolas <109356478+Neocraft1293@users.noreply.github.com>
2024-03-12 18:04:07 +0000

    Update change_theme.sh

--- change_theme.sh
+++ change_theme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Récupérer l'adresse IP publique du système
 public_ip=$(curl -s https://ipinfo.io/ip)
 
@@ -51,11 +52,17 @@ sunrise_sunset_info=$(curl -s "$api_url")
 sunrise=$(echo "$sunrise_sunset_info" | jq -r '.results.sunrise')
 sunset=$(echo "$sunrise_sunset_info" | jq -r '.results.sunset')
 
+#envoyer un message discord avec les heures de lever et de coucher du soleil
+
 # Convertir les heures de l'API Sunrise-Sunset au format 24 heures
 sunrise_24=$(date -d "$sunrise" +"%H:%M")
 sunset_24=$(date -d "$sunset" +"%H:%M")
 #recuperer l'heure actuelle par rapport au fuseau horaire $timezone
 current_time_24=$(TZ=$timezone date +"%H:%M")
+
+
+
+#envoyer un message discord avec l'heure actuelle
 echo "heure actuelle : $current_time_24"
 # Comparer les heures au format 24 heures
 if [[ "$current_time_24" > "$sunrise_24" && "$current_time_24" < "$sunset_24" ]]; then
@@ -88,9 +95,6 @@ echo "Planification de l'exécution du script pour le prochain $next_event du so
 #attendre un temp random entre 1 et une minute
 
 
-
-
-
 #gener un nombbre random entre 1 et 60
 random_number=$((1 + RANDOM % 60))
 echo "le script va attendre $random_number secondes avant de programmer la prochaine execution"
@@ -103,13 +107,6 @@ sleep $random_number
 # Vérifier si une tâche avec le nom "change_theme" existe déjà
 existing_task=$(atq | grep -F 'change_theme' | cut -f1)
 
-# Déterminer le répertoire du script
-script_dir=$(dirname "$0")
-mark_file="$script_dir/fichier_de_log" # Créer un fichier de log pour marquer la prochaine exécution du script
-# met le contenue du fichier dans une variable
-mark_file_content=$(cat "$mark_file")
-#echo $mark_file_content
-echo "mark_file_content : $mark_file_content"
 
 
 #recuperer le resultat de la commande atq
@@ -131,6 +128,20 @@ fi
 random_number=$((1 + RANDOM % 60))
 echo "le script va attendre $random_number secondes avant de programmer la prochaine execution"
 sleep $random_numbers
+
+
+
+
+
+
+
+# Déterminer le répertoire du script
+script_dir=$(dirname "$0")
+mark_file="$script_dir/fichier_de_log" # Créer un fichier de log pour marquer la prochaine exécution du script
+# met le contenue du fichier dans une variable
+mark_file_content=$(cat "$mark_file")
+#echo $mark_file_content
+echo "mark_file_content : $mark_file_content"
 
 #if mark_file_content = next_event_datetime
 if [ "$mark_file_content" = "$next_event_datetime" ]; then
